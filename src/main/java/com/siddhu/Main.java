20c76302b6614acac8899505063aac3f117df892
Siddharth <dsiddharth25@gmail.com>
2024-12-17 16:46:35 +0530

    upto tokenization

--- src/main/java/com/siddhu/Main.java
+++ src/main/java/com/siddhu/Main.java
@@ -16,17 +16,32 @@ public class Main {
 
         
         printmatrix(tk);
+        printdims(tk);
 
         
 
     }
 
     private static void printmatrix(float[][] tk) {
+        System.out.print("[");
         for (int i = 0; i < tk.length; i++) {
+            System.out.print("[");
             for (int j = 0; j < tk[i].length; j++) {
-            System.out.print(tk[i][j] + " ");
+                System.out.print(tk[i][j]);
+                if (j < tk[i].length - 1) {
+                    System.out.print(", ");
+                }
             }
-            System.out.println();
+            System.out.print("]");
+            if (i < tk.length - 1) {
+                System.out.println(",");
+            }
         }
+        System.out.println("]");
+    }
+
+    private static void printdims(float[][] tk) {
+        System.out.println("Rows: " + tk.length);
+        System.out.println("Columns: " + tk[0].length);
     }
 }
